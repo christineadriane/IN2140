fb103bf317be7f0ee1c6a9931769a5c44c3804f1
christineadriane <57804003+christineadriane@users.noreply.github.com>
2020-03-15 16:15:10 +0100

    Add files via upload

---
+++ shared.h
@@ -0,0 +1,13 @@
+#ifndef MY_SHARED_HEADER
+#define MY_SHARED_HEADER
+
+/* includes */
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+
+/* declarations */
+void read();
+void read_connections();
+
+#endif
