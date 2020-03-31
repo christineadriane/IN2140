5e0cad75f81e120f74b4b8a1069804b514952022
christineadriane <57804003+christineadriane@users.noreply.github.com>
2020-03-31 17:28:49 +0200

    Add files via upload

--- ruterdrift.c
+++ ruterdrift.c
@@ -1,81 +1,134 @@
+/* include shared header with declarations and includes */
 #include "shared.h"
 
+// struct with id, flag, model lenght, model name
 struct router{
   unsigned int id;
   unsigned char flag;
   unsigned char mLenght;
   char model[248];
 
-  int nCounter; // Antall naboer
-  struct router* nList[10];
+  int nCounter; // number of neighbours
+  struct router* nList[10]; // list of neighbours
 };
 
 struct router** routerPointer;
 
 int main(int argc, char const *argv[]) {
 
+  // Opening file with first argument, read binary
   FILE *myFile = fopen(argv[1], "rb");
   if (myFile == NULL){
     perror("Nej");
     return EXIT_FAILURE;
   }
+
+  // keeping track of number of routers by reading the first int
   int routers;
   fread(&routers, sizeof(int), 1, myFile);
   printf("Number of routers: %d\n", routers);
 
+  // allocate space for router pointers
   routerPointer = malloc((sizeof(struct router*)) * routers); //8 * routers byte
 
-  for (int i = 0; i < routers; i++){
+  int i;
+  for (i = 0; i < routers; i++){
     read(myFile, i);
   }
 
   // Tester innlesing av øvrig infoblokker
   read_connections(myFile, routers);
 
-  // Tester å legge til en kobling
-  add_connection(routers, 9, 1);
-  printf("\nRuter 9 antall naboer: %d\n", routerPointer[9]->nCounter);
-  printf("\nRuter 9 sin nabo: %d\n", routerPointer[9]->nList[0]->id);
-
-  add_connection(routers, 6, 1);
-
-  printf("\nRuter 6 sine naboer: %d, %d, %d, %d\n", routerPointer[6]->nList[0]->id, routerPointer[6]->nList[1]->id, routerPointer[6]->nList[2]->id, routerPointer[6]->nList[3]->id);
-
-  // Tester om ting kobles sammen
-  printf("\nHEI: %d\n", routerPointer[6]->nList[2]->id);
-
-  //Tester å bytte modellnavn
-  set_model(routers, 0, "Fin ny ruter");
-  printf("\nNytt navn: %s\n", routerPointer[0]->model);
-
-  // Tester print
-  print(routers, 6);
-
-
-  // Tester sletting
-  delete(routers, 2);
-  printf("Naboen til 6: %u\n", routerPointer[5]->nList[1]->id);
-  printf("ID: %d\n", routerPointer[2]->id);
-
   // Lukker fil
   fclose(myFile);
 
   // Start reading argv[2]
-
-  myFile = fopen(argv[2], "r");
-  if (myFile == NULL){
+  FILE *newFile = fopen(argv[2], "r");
+  if (newFile == NULL){
     perror("Nej");
     return EXIT_FAILURE;
   }
-  printf("File: %s\n", argv[2]);
+  printf("Reading from file: %s\n", argv[2]);
 
-  //Print, set flag, set model, add connection, delete, existing
+  char readLine[248];
+  char* string;
 
-  fclose(myFile);
+  while (fgets(readLine, 248, newFile)!= NULL){
+    string = strtok(readLine, " ");
+
+    // print
+    if(strcmp(string, "print") == 0){
+      string = strtok(NULL, " ");
+      print(routers, atoi(string));
+      printf("\n");
+    }
+
+    // set model
+    if(strcmp(string, "sett_modell") == 0){
+      string = strtok(NULL, " ");
+      char* name = strtok(NULL, "\n");
+      set_model(routers, atoi(string), name);
+    }
+
+    // add connection
+    if(strcmp(string, "legg_til_kobling") == 0){
+      string = strtok(NULL, " ");
+      int id = atoi(strtok(NULL, " "));
+      add_connection(routers, atoi(string), id);
+      printf("\n");
+    }
+
+    // delete router
+    if(strcmp(string, "slett_router") == 0){
+      string = strtok(NULL, " ");
+      delete(routers, atoi(string));
+    }
+
+    // set flag
+    if(strcmp(string, "sett_flag") == 0){
+      string = strtok(NULL, "\n");
+    }
+  }
+  // close file
+  fclose(newFile);
+
+  // write to file
+  FILE *test = fopen("test.txt", "w");
+  if (test == NULL){
+    perror("Nej");
+    return EXIT_FAILURE;
+  }
+  // If there is a new number of routers after some is deleted.
+  int newRouters;
+  int a;
+  for(a = 0; a < routers; a++){
+    struct router* r = routerPointer[a];
+    if (r != NULL){
+      newRouters++;
+    }
+  }
+  // write the first 4 bytes with number of routers
+  fwrite(&newRouters, sizeof(int), 1, myFile);
+
+  // write information about the routers
+  for(a = 0; a < routers; a++){
+    struct router* r = routerPointer[a];
+    if (r != NULL){
+      fwrite(&r->id, sizeof(int), 1, test);
+      fwrite(&r->flag, sizeof(char), 1, test);
+      fwrite(&r->mLenght, sizeof(char), 1, test);
+      fwrite(&r->model, routerPointer[a]->mLenght - sizeof(char), 1, test);
+      fwrite("\n", sizeof(char), 1, test);
+    }
+  }
+  // closing the test file
+  fclose(test);
+
+  // free all the allocated memory
+  free_memory(routers);
 }
 
-// Innlesing til struct
-// Ferdig
+// method to read info to struct
 void read(FILE *myFile, int n){
 
   struct router* rp = malloc(sizeof(struct router)); //256
@@ -85,17 +138,19 @@ void read(FILE *myFile, int n){
   fread(&rp -> mLenght, sizeof(char), 1, myFile);
   fread(&rp -> model, rp -> mLenght + 1, 1, myFile);
 
+  /*  Tester print
   printf("Model: %s\n", rp -> model);
   printf("ID: %d\n", rp -> id);
   printf("Flag: %d\n", rp -> flag);
-  printf("Lengde: %d\n", rp -> mLenght);
+  //printf("Lengde: %d\n", rp -> mLenght);
   printf("\n");
+  */
 
   routerPointer[n]=rp;
 }
 
-// Leser inn og legger inn connections
-// Legger til naboteller
+// reading and adding connections
+// also increased neighbour counter
 void read_connections(FILE* myFile, int routers){
   unsigned int ru1;
   unsigned int ru2;
@@ -116,14 +171,17 @@ void read_connections(FILE* myFile, int routers){
     r1->nList[r1->nCounter] = r2;
     r1->nCounter++;
 
+    /* Tester print av koblingene
     printf("%d\n", ru1);
     printf("%d\n", ru2);
+    */
   }
 }
 
-// General methis to find a router based on ID
+// General method to find a router based on ID
 struct router* find_router(int routers, int id){
-  for(int i = 0; i < routers; i++){
+  int i;
+  for(i = 0; i < routers; i++){
     if(routerPointer[i]->id == id){
       return routerPointer[i];
     }
@@ -137,7 +195,8 @@ struct router* find_router(int routers, int id){
 
 // Free Memory - free allocated memory to avoid leaks
 void free_memory(int routers) {
-  for(int i = 0; i < routers; i++) {
+  int i;
+  for(i = 0; i < routers; i++) {
     struct router* r = routerPointer[i];
       if (r != NULL) {
         free(r); // Free 256 Bytes
@@ -148,8 +207,8 @@ void free_memory(int routers) {
     return;
 }
 
-// Set modell
-// Funker slik den skal
+// set modell
+// change the name of an existing router
 void set_model(int routers, int id, char* name){
   if (find_router(routers, id) == NULL){
     printf("Router: %d does not exist.\n", id);
@@ -158,9 +217,10 @@ void set_model(int routers, int id, char* name){
   struct router* routerPointer = find_router(routers, id);
   strcpy(routerPointer->model, name);
   routerPointer -> mLenght = strlen(name)+1;
+  printf("Router %d successfully changed name to '%s'\n", id, name);
 }
 
-// Add connections
+// add connections
 /*
 Metoden funker. Burde ta hensyn til og legge til at den sjekker tilkoblinger som allerede finnes
 */
@@ -177,10 +237,12 @@ void add_connection(int routers, int id, int id2){
   struct router* r2 = (find_router(routers, id2));
   r1->nList[r1->nCounter] = r2;
   r1->nCounter++;
+
+  printf("New connection: %d -> %d\n", id, id2);
+
 }
 
 // Ferdig kommando, printe bitwise på flagg?
-// Gjør det ved overskudd
 void print(int routers, int id){
   if (find_router(routers, id) == NULL){
     printf("Router: %d does not exist.\n", id);
@@ -188,51 +250,62 @@ void print(int routers, int id){
   }
 
   struct router* routerPointer = find_router(routers, id);
+  printf("\nPrinting info about router %d:\n", id);
   printf("Model: %s\n", routerPointer-> model);
   printf("ID: %d\n", routerPointer -> id );
-  printf("Flag: %d\n", routerPointer -> flag);
+  //printf("Flag: %d\n", routerPointer -> flag);
+  printf("Flag: ");
+  binary_print(routerPointer->flag);
 
-  printf("\nNaboer:\n");
+  printf("\nNeighbours:\n");
 
-  // Finner naboer i nList og printer id og modell
-  for(int i = 0; i < routerPointer->nCounter; i++){
-    printf("Router: %d\nModel: %s\n", routerPointer-> nList[i]->id, routerPointer->nList[i]->model);
+  // finding neighbours in nList and printer their id and model
+  int i;
+  for(i = 0; i < routerPointer->nCounter; i++){
+    printf("Router: %d - %s\n", routerPointer-> nList[i]->id, routerPointer->nList[i]->model);
   }
 }
 
-
-// Slette ruter
-// FERDIG
+// delete router
+// removing all pointers to said routers, decreasing nCounter for the previous neighbours
 void delete(int routers, int id){
   if (find_router(routers, id) == NULL){
     printf("Router: %d does not exist.\n", id);
     return;
   }
 
-  for(int i = 0; i < routers; i++){
+  int i;
+  for(i = 0; i < routers; i++){
     struct router* rp = find_router(routers, i);
-    for (int j = 0; j < rp->nCounter; j++){
+    int j;
+    for (j = 0; j < rp->nCounter; j++){
       if (rp->nList[j]->id == id){
-        for(int k = j; k < rp->nCounter - 1; k++){
+        int k;
+        for(k = j; k < rp->nCounter - 1; k++){
           rp->nList[k] = rp->nList[k+1];
         }
         rp->nCounter --;
       }
     }
   }
+
   int l;
   for(l = 0; l < routers -1; l++){
     if(routerPointer[l]->id == id){
-      printf("l: %d\n", l);
       break;
     }
   }
   int m;
   for(m = l; m < routers - 1; m++){
-    printf("m: %d\n", m);
-    printf("Router 1: %d\n", routerPointer[m]->id);
-    printf("Ruter 2: %d\n", routerPointer[m +1 ]->id);
     routerPointer[m] = routerPointer[m + 1];
-    printf("Router hei: %d\n", routerPointer[m]->id);
   }
+  printf("Deleted router: %d\n", id);
+}
+
+void binary_print(char b){
+  int i;
+  for (i = 7; i >= 0; i--){
+    printf("%d", (b >> i) & 0x01);
+  }
+  printf("\n");
 }
